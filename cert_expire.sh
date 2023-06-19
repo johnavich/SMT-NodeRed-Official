6e54c385d0129ab1010dbd7e83399825b349dca2
johnavich <37229753+johnavich@users.noreply.github.com>
2023-06-18 20:26:32 -0500

    Update cert_expire.sh
    changed date calculation, makes script just ever so slightly faster

--- cert_expire.sh
+++ cert_expire.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-DTE=$(date '+%Y-%m-%d %T')
+#DTE=$(date '+%Y-%m-%d %T')
+printf -v DTE '%(%F %T)T' -1
 if ! [ -x "$(command -v openssl)" ];
 then
     # Looked for, and did not find openssl.  This is required to determine if the certificate is a good certificate.
@@ -7,12 +8,11 @@ then
     echo "$DTE Openssl not installed. Installing." > /config/logs/cert.log
     apk add openssl
 fi
+printf -v DTE '%(%F %T)T' -1
 if $(openssl x509 -in /media/fullchain.pem -noout -checkend 86400 >/dev/null)
 then
-    DTE=$(date '+%Y-%m-%d %T')
     echo "$DTE Cert not exipring" >> /config/logs/cert.log
 else
-    DTE=$(date '+%Y-%m-%d %T')
     echo "$DTE Cert expiring" >> /config/logs/cert.log
     cp /ssl/fullchain.pem /media/fullchain.pem
     cp /ssl/privkey.pem /media/privkey.pem
