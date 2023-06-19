58fbf1a5dce1c54f116323c866a6b9da0c455111
johnavich <37229753+johnavich@users.noreply.github.com>
2023-06-18 20:20:10 -0500

    Update cert_expire.sh
    Added openssl info and logic

--- cert_expire.sh
+++ cert_expire.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-if ! $(openssl x509 -in /media/fullchain.pem -noout -checkend 86400 >/dev/null)
+DTE=$(date '+%Y-%m-%d %T')
+if ! [ -x "$(command -v openssl)" ];
 then
+    # Looked for, and did not find openssl.  This is required to determine if the certificate is a good certificate.
+    # HA removed openssl from their base image, so we need to add it back. This will install it automatically every time, even after an update.
+    echo "$DTE Openssl not installed. Installing." > /config/logs/cert.log
+    apk add openssl
+fi
+if $(openssl x509 -in /media/fullchain.pem -noout -checkend 86400 >/dev/null)
+then
+    DTE=$(date '+%Y-%m-%d %T')
+    echo "$DTE Cert not exipring" >> /config/logs/cert.log
+else
+    DTE=$(date '+%Y-%m-%d %T')
+    echo "$DTE Cert expiring" >> /config/logs/cert.log
     cp /ssl/fullchain.pem /media/fullchain.pem
     cp /ssl/privkey.pem /media/privkey.pem
 fi
